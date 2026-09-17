965779af1e4c2baa6af361a789bad0a2790f9ee0
NaimMushy <bady.iris@gmail.com>
2026-09-17 17:13:53 +0200

    feat: finished with Texture_pack class for now

--- src/view/Texture_pack.py
+++ src/view/Texture_pack.py
@@ -1,17 +1,21 @@
+import os
+from pathlib import Path
+
+
+DEFAULT_PACK: str = "src/view/assets/default_texture_pack/"
+
+
 class Texture_pack:
+
     def __init__(self, pack_path: str = DEFAULT_PACK) -> None:
 
         self._load_pack(pack_path)
-
-    @staticmethod
-    def check_missing_textures(pack_path: str) -> bool:
-
-        folders_needed: list[str] = [
+        self.folders_needed: list[str] = [
             "pac-man_textures",
             "ghosts_textures",
             "fruits_textures"
         ]
-        textures_needed: list[str] = [
+        self.textures_needed: list[str] = [
             "pac-man_closed",
             "pac-man_open",
             "blinky_right",
@@ -39,54 +43,41 @@ class Texture_pack:
             "bell",
             "key"
         ]
-        folders = {f.name: f.path for f in os.scandir(pack_path) if f.is_dir()}
-
-        if not folders or folders != folders_needed:
-            return False
-
-        self.all_textures = {texture_name: "" for texture_name in textures_needed}
-        for folder in folders.values():
-            textures_found += rglob.find(folder, ".png", "jpg")
-
-        if textures_found != textures_needed:
-            return False
-
-
 
     def _load_pack(self, pack_path: str) -> None:
 
-        folders = {f.name: f.path for f in os.scandir(pack_path) if f.is_dir()}
+        folders = {
+            f.name: f.path for f in os.scandir(pack_path)
+            if f.is_dir() and f.name in self.folders_needed
+        }
 
-        self.pac_man: Pacman_text = Pacman_text(folders["pac-man_textures"])
+        self.all_textures: dict = {}
+        for folder in folders.values():
+            self.all_textures.update({
+                t.name.split(".")[0]: pack_path + t.path
+                for t in os.scandir(folder)
+                if t.is_file() and t.name.split(".")[0] in self.textures_needed
+            })
 
-        ghosts_files = 
-        self.blinky: Ghost_text = Ghost_text(folders["ghosts_textures"])
-        self.textures: dict[str, str] = {
-            image_path for image_path in [
-                "pac-man_closed",
-                "pac-man_open",
-                "blinky_right",
-                "blinky_left",
-                "blinky_up",
-                "blinky_down",
-                "pinky_right",
-                "pinky_left",
-                "pinky_up",
-                "pinky_down",
-                "inky_right",
-                "inky_left",
-                "inky_up",
-                "inky_down",
-                "clyde_right",
-                "clyde_left",
-                "clyde_up",
-                "clyde_down",
-                "apple",
-                "strawberry",
-                "orange",
-                "cherry",
-                "melon",
-                "galaxian",
-                "bell",
-                "key"
-                }
+        textures_missing: list = [
+            missing_text for missing_text in self.textures_needed
+            if missing_text not in self.all_textures.keys()
+        ]
+
+        if not textures_missing:
+            return
+
+        self.get_missing_textures(textures_missing)
+
+    def get_missing_textures(self, textures_missing: list[str]) -> None:
+
+        for mis_text in textures_missing:
+
+            self.all_textures[mis_text] = Path(DEFAULT_PACK).rglob(mis_text)
+
+    def get_texture(self, texture_name: str) -> str:
+
+        if texture_name not in self.all_textures:
+            raise ValueError(f"Missing texture {texture_name} in texture pack")
+
+        return str(self.all_textures[texture_name])
