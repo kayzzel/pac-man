4bcaef6a29ef11e0ee2569d4f02a85ef63595e95
Iris Bady <ibady@z1r6p3.42lyon.fr>
2026-09-21 10:52:27 +0200

    feat: continued working on building the display map

--- src/view/Game_view.py
+++ src/view/Game_view.py
@@ -29,6 +29,16 @@ DIR_KEYS: list = [
 ]
 
 
+class Wall:
+
+    def __init__(self) -> None:
+
+        self.north: bool = False
+        self.south: bool = False
+        self.west: bool = False
+        self.east: bool = False
+
+
 class Game_view(View):
 
     def __init__(self, app: Any, game: Game, modal_view: View) -> None:
@@ -91,6 +101,19 @@ class Game_view(View):
             True
         ) for life in range(self.game.player.nb_lives)]
 
+    @property
+    def map_startx(self) -> int:
+
+        return (
+            self.left_panel_width + MIDDLE_LINE_THICKNESS
+            + (self.map_panel_width - self.map_width) // 2
+        )
+
+    @property
+    def map_starty(self) -> int:
+
+        return (self.h - self.map_height) // 2
+
     def _update_map_panel(self) -> None:
 
         if pr.is_key_pressed(pr.KEY_UP) or pr.is_key_pressed(pr.KEY_W):
@@ -103,19 +126,15 @@ class Game_view(View):
             self.game.player.direction = "E"
 
         self.map_panel_width: int = self.w - self.left_panel_width - 10
-        self.map_size: int = self.h - self.h // 10
-        self.map_startx: int = self.left_panel_width + MIDDLE_LINE_THICKNESS + (self.map_panel_width - self.map_size) // 2
-        self.map_starty: int = self.h // 20
-        self.map_outline: tuple[int, int, int, int] = (
-            self.map_startx,
-            self.map_starty,
-            self.map_size,
-            self.map_size
+        self.map_width: int = min(
+            self.map_panel_width - self.map_panel_width // 10,
+            self.h - self.h // 10
         )
-        pac_man_size: int = self.map_size // 16
+        self.map_height: int = self.map_width
+        pac_man_size: int = self.map_width // 16
 
         self.pacman_sprite: AnimIcon = AnimIcon(
-            self.map_startx + (self.map_size - pac_man_size) // 2,
+            self.map_startx + (self.map_width - pac_man_size) // 2,
             -2,
             self.texture_pack.get_texture(
                 "pac-man_" + DIRECTIONS[self.game.player.direction]
@@ -123,29 +142,150 @@ class Game_view(View):
             (pac_man_size, pac_man_size),
             True
         )
-        self.calculate_cell_size()
+        self.grid: list[list] = self.game.map.cells
+        self.insert_rows()
+        self.insert_cols()
 
-    def calculate_cell_size(self) -> None:
+        nb_cells_row: int = self.map_width // len(self.grid[0])
+        nb_cells_col: int = self.map_height // len(self.grid)
 
-        ver_walls: int = 0
-        hor_walls: int = 0
-        nb_cells: int = len(self.game.map.cells)
+        if nb_cells_row > nb_cells_col:
+            self.cell_size: int = nb_cells_row
+            self.map_height = self.cell_size * len(self.grid)
+        else:
+            self.cell_size = nb_cells_col
+            self.map_width = self.cell_size * len(self.grid[0])
 
-        for cell in self.game.map.cells:
+        self.map_outline: tuple[int, int, int, int] = (
+            self.map_startx,
+            self.map_starty,
+            self.map_width,
+            self.map_height
+        )
 
-            if cell.walls["north"]:
-                ver_walls += 1
-            if cell.walls["south"]:
-                ver_walls += 1
-            if cell.walls["west"]:
-                hor_walls += 1
-            if cell.walls["east"]:
-                hor_walls += 1
+    def insert_rows(self) -> None:
 
-        map_width: int = nb_cells + hor_walls
-        map_height: int = nb_cells + ver_walls
+        self.north_rows: list[tuple[list, int]] = []
+        self.south_rows: list[tuple[list, int]] = []
 
-        self.cell_size: int = min(self.map_size // map_width, self.map_size // map_height)
+        for r in range(len(self.grid)):
+
+            row = self.grid[r]
+
+            north_row: list = []
+            south_row: list = []
+
+            for cell in range(len(row)):
+
+                if row[cell].walls["N"]:
+                    if not north_row:
+                        north_row = [
+                            Wall()
+                            for _ in range(len(row))
+                        ]
+                    north_row[cell].north = True
+
+                if row[cell].walls["S"]:
+                    if not south_row:
+                        south_row = [
+                            Wall()
+                            for _ in range(len(row))
+                        ]
+                    south_row[cell].south = True
+
+            if north_row:
+                self.north_rows.append((north_row, r))
+            if south_row:
+                self.south_rows.append((south_row, r + 1 + (
+                    1 if north_row else 0
+                )))
+
+        for n_row, index in self.north_rows:
+            self.grid.insert(index, n_row)
+        for s_row, index in self.south_rows:
+            self.grid.insert(index, s_row)
+
+    def insert_cols(self) -> None:
+
+        self.west_cols: list[tuple[list, int]] = []
+        self.east_cols: list[tuple[list, int]] = []
+
+        for c in range(len(self.grid[0])):
+
+            west_col: list = []
+            east_col: list = []
+
+            for r in range(len(self.grid)):
+
+                if any(
+                    isinstance(cell, Wall)
+                    for cell in self.grid[r]
+                ):
+                    continue
+
+                if self.grid[r][c].walls["W"]:
+                    if not west_col:
+                        west_col = [
+                            Wall()
+                            for _ in range(len(self.grid))
+                        ]
+                    west_col[r].west = True
+
+                if self.grid[r][c].walls["E"]:
+                    if not east_col:
+                        east_col = [
+                            Wall()
+                            for _ in range(len(self.grid))
+                        ]
+                    east_col[r].east = True
+
+            if west_col:
+                self.west_cols.append((west_col, c))
+            if east_col:
+                self.east_cols.append((east_col, c + 1 + (
+                    1 if west_col else 0
+                )))
+
+        for w_col, index in self.west_cols:
+            for row_index in range(len(self.grid)):
+                self.grid[row_index].insert(w_col[row_index], index)
+        for e_col, index in self.east_cols:
+            for row_index in range(len(self.grid)):
+                self.grid[row_index].insert(e_col[row_index], index)
+
+    def draw_wall(self) -> None:
+
+        pr.draw_rectangle(
+            self.cur_x,
+            self.cur_y,
+            self.cell_size,
+            self.cell_size,
+            pr.DARKBLUE
+        )
+
+    def draw_cell(self) -> None:
+
+        ...
+
+    def draw_grid(self) -> None:
+
+        self.cur_y: int = self.map_starty
+
+        for row in self.grid:
+
+            self.cur_x: int = self.map_startx
+
+            for square in row:
+
+                if isinstance(square, Wall):
+                    self.draw_wall()
+
+                else:
+                    self.draw_cell()
+
+                self.cur_x += self.cell_size
+
+            self.cur_y += self.cell_size
 
     def _update(self) -> None:
 
@@ -179,6 +319,8 @@ class Game_view(View):
         )
 
         self.pacman_sprite.display_widget()
+
+        self.draw_grid()
 
         for label, coor in self.labels_data.items():
 
