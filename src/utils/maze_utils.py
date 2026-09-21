991e7040a439784584cd09bdaa4ea9f0360e9a36
kayzzel <k4yzzel@gmail.com>
2026-09-21 16:06:44 +0200

    feat: started doing the convertion from maze to a pacman map

---
+++ src/utils/maze_utils.py
@@ -0,0 +1,42 @@
+from ..Config import Config
+from ..game.map.Map import Map
+from ..game.map.Cell import Cell
+from ..game.entity.collectible.Collectible import Collectible
+
+def maze_center(height: int, width: int):
+    return ()
+
+def convert_maze_to_map(maze: list[list[int]], config: Config) -> Map:
+    new_map: Map = Map()
+
+    width = len(maze[0])
+    height = len(maze)
+
+    def convert_nbr_to_cell(nbr: int) -> dict[str, bool]:
+        return {
+                "north": bool(nbr & 1),
+                "east": bool(nbr & 2),
+                "south": bool(nbr & 4),
+                "west": bool(nbr & 8),
+        }
+
+    for y in range(height):
+        for x in range(width):
+            cell = Cell(x, y)
+            cell.walls = convert_nbr_to_cell(maze[y][x])
+
+            if ((x == 0 or x == width - 1) and (y == 0 or y == height - 1)):
+                cell.collectible = Collectible(
+                        "super_pacgum", config.point_per_super_pacgum
+                    )
+            else:
+                cell.collectible = Collectible(
+                        "pacgum", config.point_per_pacgum
+                    )
+
+            new_map.cells.append(cell)
+    new_map.collectible_count = width * height - 1
+            
+    return new_map
+
+
