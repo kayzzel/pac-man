bc395860158b180fe158cae87de7ec1e3dc7fc1f
kayzzel <k4yzzel@gmail.com>
2026-09-22 21:33:21 +0200

    feat: changed the 1D cell array in the map to a 2D cell array

--- src/game/map/Map.py
+++ src/game/map/Map.py
@@ -3,6 +3,6 @@ from .Cell import Cell
 
 class Map:
     def __init__(self) -> None:
-        self.cells: list[Cell] = []
+        self.cells: list[list[Cell]] = []
         self.collectible_count: int = 0
         self.spawn: tuple[int, int] = (0, 0)
