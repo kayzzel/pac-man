5c1a7414c9c0e40997a769e53eeaddd7fbfadab5
kayzzel <k4yzzel@gmail.com>
2026-09-25 15:53:09 +0200

    feat: added the Entity class to pacman

--- src/game/entity/Pac_man.py
+++ src/game/entity/Pac_man.py
@@ -1,12 +1,11 @@
+from .Entity import Entity
 from ...Config import Config
 from .collectible.Collectible import Collectible
 
 
-class Pac_man:
+class Pac_man(Entity):
     def __init__(self, config: Config) -> None:
+        super().__init__()
         self.score = 0
         self.nb_lives = config.lives
-        self.pos_x: float = 0.0
-        self.pos_y: float = 0.0
-        self.direction = "N"
         self.collectible: list[None | Collectible] = []
